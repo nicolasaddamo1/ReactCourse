9f600b67cef61164a4f097c7259b0752f4f8d84e
Nico <nicolasaddamo1@gmail.com>
2025-03-25 17:53:52 -0300

    added useState

--- 03-contador/src/counter/Counter.jsx
+++ 03-contador/src/counter/Counter.jsx
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 function Counter() {
+    const [count, setCount] = useState(0);
+
     return (
         <>
-            <p>Haz hecho click 0 veces en el botón</p>
+            <p>Haz hecho click {count} veces en el botón</p>
             <button>Haz click</button>
         </>
     )
