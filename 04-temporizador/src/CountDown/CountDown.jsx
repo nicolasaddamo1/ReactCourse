15e2104a5a0f95aaeb95ab35c66c5241fa541225
Nico <nicolasaddamo1@gmail.com>
2025-03-27 18:29:31 -0300

    finished conteo

--- 04-temporizador/src/CountDown/CountDown.jsx
+++ 04-temporizador/src/CountDown/CountDown.jsx
@@ -1,18 +1,38 @@
+import { useEffect } from 'react';
 import { useState } from 'react';
 function CountDown() {
     const [seconds, setSeconds] = useState(null);
     const [elapsedSeconds, setElapsedSeconds] = useState(0);
+
+    useEffect(function () {
+        if (seconds === null) return;
+        setElapsedSeconds(0);
+        let interval = setInterval(() => {
+            setElapsedSeconds((elapsedSeconds) => elapsedSeconds + 1);
+
+        }, 1000);
+    }, [seconds])
     function parseForm(event) {
         event.preventDefault();
         let secs = event.target.secs.value;
         setSeconds(parseInt(secs));
     }
 
+    if (elapsedSeconds >= seconds && seconds !== null) {
+        return (
+            <>
+                <p className="timer">
+                    Conteo Terminado
+                </p>
+                <button onClick={() => setSeconds(null)}>Reiniciar</button>
+            </>
+        )
+    }
     if (seconds !== null) {
         return (
             <>
                 <p className="timer">
-                    Conteo desde el {seconds}
+                    Faltan {seconds - elapsedSeconds} segundos para que termine la cuenta atrás
                 </p>
             </>
         )
