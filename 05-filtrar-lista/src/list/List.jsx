c92aa3522553162f3d411ebb4fd0bad162816625
Nico <nicolasaddamo1@gmail.com>
2025-03-27 20:08:09 -0300

    almost there

--- 05-filtrar-lista/src/list/List.jsx
+++ 05-filtrar-lista/src/list/List.jsx
@@ -1,9 +1,24 @@
 import ListView from "./ListView";
 import frameworksList from './items.js'
+import { useState } from "react";
 function List() {
+    const [items, setItems] = useState(frameworksList);
+
+    function functionFilterItems(searchPattern) {
+        if (searchPattern === '') {
+            setItems(frameworksList);
+        } else {
+            let newItems = filterItemsBySearchPattern(searchPattern);
+            setItems(newItems);
+        }
+    }
+    function filterItemsBySearchPattern(searchPattern) {
+        const filterItems = frameworksList.filter(item => item.toLowerCase().includes(searchPattern.toLowerCase()));
+        return filterItems;
+    }
     return (
         <div className="list">
-            <ListView elements={frameworksList} />
+            <ListView elements={items} functionFilterItems={functionFilterItems} />
         </div>
     )
 }
--- 05-filtrar-lista/src/list/ListView.jsx
+++ 05-filtrar-lista/src/list/ListView.jsx
@@ -1,7 +1,9 @@
-function ListView(elements) {
+function ListView(elements, functionFilterItems) {
     return (
         <>
-            <input type="text" placeholder="Filtrar" ></input>
+            <input type="text" placeholder="Filtrar"
+                onChange={ev => functionFilterItems(ev.target.value)} />
+
             <ul>
                 {elements.elements.map((element) => (
                     <li >{element}</li>
