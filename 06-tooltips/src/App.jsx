c613b6dd55f3015af0044dfef22027b8f5897ab5
Nico <nicolasaddamo1@gmail.com>
2025-04-03 20:03:10 -0300

    modified app

--- 06-tooltips/src/App.jsx
+++ 06-tooltips/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import TooltipText from './tooltip/TooltipText'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <div className="App">
+        <p><TooltipText tooltip={"Texto generado"}>Lorem ipsum</TooltipText> dolor sit amet, consectetur adipiscing elit </p>
+        <p>Si longus, levis dictata sunt. De quibus cupio scire quid sentias. Eam tum adesse, cum dolor sunt aenean</p>
+        <p>Videsne quam sit magna dissensio? Quod quidem iam fit etiam in <TooltipText tooltip={"Esto es lo que hay aqui."}>Academia</TooltipText></p>
+      </div>
     </>
   )
 }
