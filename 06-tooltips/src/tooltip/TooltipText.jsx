616757212dac35575dbdac3d056aff710da80f21
Nico <nicolasaddamo1@gmail.com>
2025-04-03 20:13:59 -0300

    saved before of useRef

--- 06-tooltips/src/tooltip/TooltipText.jsx
+++ 06-tooltips/src/tooltip/TooltipText.jsx
@@ -1,7 +1,13 @@
+import { useRef } from "react";
+
 function TooltipText(props) {
+    const spanElement = useRef()
+    function handleMouseOver(ev) {
+
+    }
     return (
         <>
-            <span className="tooltip-text">
+            <span className="tooltip-text" ref={spanElement} onMouseOver={ev => handleMouseOver(ev)}>
                 {props.children}
             </span>
         </>
