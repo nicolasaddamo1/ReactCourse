192b930aa272ea36db34dcff71fc822251d6d769
Nico <nicolasaddamo1@gmail.com>
2025-04-03 21:00:47 -0300

    added style

--- 06-tooltips/src/tooltip/Tooltip.jsx
+++ 06-tooltips/src/tooltip/Tooltip.jsx
@@ -4,13 +4,14 @@ function Tooltip({ text, domRect }) {
     const [position, setPosition] = useState({ x: 0, y: 0 })
     const tooltipElement = useRef()
     useEffect(() => {
+        const { width, height } = tooltipElement.current.getBoundingClientRect()
         setPosition({
-            y: domRect.y,
-            x: domRect.x
+            y: domRect.y - (height + 10),
+            x: domRect.x + (domRect.width / 2) - (width / 2)
         })
     }, [domRect])
     return (
-        <span className="tooltip" ref={tooltipElement}>{text}</span>
+        <span className="tooltip" ref={tooltipElement} style={{ top: position.y, left: position.x }}>{text}</span>
     )
 
 }
